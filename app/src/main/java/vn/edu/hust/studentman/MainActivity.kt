9095f2f1fcc2e95360bd18e85dc935236c46ea81
phhuy06 <pqh0612@gmail.com>
2024-11-14 11:47:53 +0700

    20225338

--- app/src/main/java/vn/edu/hust/studentman/MainActivity.kt
+++ app/src/main/java/vn/edu/hust/studentman/MainActivity.kt
@@ -1,15 +1,19 @@
 package vn.edu.hust.studentman
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var studentAdapter: StudentAdapter
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
@@ -37,11 +41,99 @@ class MainActivity : AppCompatActivity() {
       StudentModel("Lê Văn Vũ", "SV020")
     )
 
-    val studentAdapter = StudentAdapter(students)
+    studentAdapter = StudentAdapter(
+      students,
+      onEditClick = { student ->
+        openEditStudentDialog(studentAdapter, students, student)
+      },
+      onRemoveClick = { student ->
+        openRemoveStudentDialog(studentAdapter, students, student)
+      }
+    )
 
     findViewById<RecyclerView>(R.id.recycler_view_students).run {
       adapter = studentAdapter
       layoutManager = LinearLayoutManager(this@MainActivity)
     }
+
+    val addNewButton = findViewById<Button>(R.id.btn_add_new).setOnClickListener {
+      openAddStudentDialog(studentAdapter, students)
+    }
+  }
+
+  private fun openAddStudentDialog(adapter: StudentAdapter, students: MutableList<StudentModel>) {
+    val dialogView = layoutInflater.inflate(R.layout.dialog_add_student, null)
+
+    val nameEditText = dialogView.findViewById<EditText>(R.id.edit_text_student_name)
+    val idEditText = dialogView.findViewById<EditText>(R.id.edit_text_student_id)
+
+    AlertDialog.Builder(this)
+      .setView(dialogView)
+      .setTitle("Add New Student")
+      .setPositiveButton("Add") { dialogInterface, _ ->
+        val name = nameEditText.text.toString().trim()
+        val id = idEditText.text.toString().trim()
+
+        if (name.isNotEmpty() && id.isNotEmpty()) {
+          val newStudent = StudentModel(name, id)
+          students.add(newStudent)
+          adapter.notifyItemInserted(students.size - 1)
+          dialogInterface.dismiss()
+        } else {
+          if (name.isEmpty()) nameEditText.error = "Enter student name"
+          if (id.isEmpty()) idEditText.error = "Enter student ID"
+        }
+      }
+      .setNegativeButton("Cancel") { dialogInterface, _ ->
+        dialogInterface.dismiss()
+      }
+      .create().show()
+  }
+
+  private fun openEditStudentDialog(adapter: StudentAdapter, students: MutableList<StudentModel>, student: StudentModel) {
+    val dialogView = layoutInflater.inflate(R.layout.dialog_add_student, null)
+    val nameEditText = dialogView.findViewById<EditText>(R.id.edit_text_student_name)
+    val idEditText = dialogView.findViewById<EditText>(R.id.edit_text_student_id)
+
+    nameEditText.setText(student.studentName)
+    idEditText.setText(student.studentId)
+
+    AlertDialog.Builder(this)
+      .setView(dialogView)
+      .setTitle("Edit Student")
+      .setPositiveButton("Save") { dialogInterface, _ ->
+        val newName = nameEditText.text.toString().trim()
+        val newId = idEditText.text.toString().trim()
+
+        if (newName.isNotEmpty() && newId.isNotEmpty()) {
+          val index = students.indexOf(student)
+          students[index] = StudentModel(newName, newId)
+          adapter.notifyItemChanged(index)
+        }
+        dialogInterface.dismiss()
+      }
+      .setNegativeButton("Cancel") { dialogInterface, _ ->
+        dialogInterface.dismiss()
+      }
+      .create().show()
+  }
+
+  private fun openRemoveStudentDialog(adapter: StudentAdapter, students: MutableList<StudentModel>, student: StudentModel) {
+    AlertDialog.Builder(this)
+      .setTitle("Remove Student")
+      .setMessage("Are you sure you want to remove ${student.studentName}?")
+      .setPositiveButton("Remove") { dialogInterface, _ ->
+        val index = students.indexOf(student)
+        if (index != -1) {
+          students.removeAt(index)
+          adapter.notifyItemRemoved(index)
+        }
+        dialogInterface.dismiss()
+      }
+      .setNegativeButton("Cancel") { dialogInterface, _ ->
+        dialogInterface.dismiss()
+      }
+      .create()
+      .show()
   }
 }
